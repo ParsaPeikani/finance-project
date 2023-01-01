d2cf824b454356e8ea584cb75aa7ff82df751901
Parsa Peikani <104706652+Weekendend@users.noreply.github.com>
2023-01-01 00:17:26 -0800

    Making the Analyze expenses page functional

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -19,6 +19,10 @@ function ExpenseForm() {
   ).data;
   const expenseData = expenses.find((expense) => expense.id === params.id);
 
+  if (params.id && !expenseData) {
+    return <p>Invalid expense Id.</p>;
+  }
+
   const navigation = useNavigation();
 
   const defaultValues = expenseData
